f87188e21effa34e73349e04a6252eed789fb320
RAHUL <yalavattirahul@gmail.com>
2025-06-20 12:13:04 +0530

    pipe

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -119,6 +119,8 @@ class ModelTrainer:
 
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path,obj=NetworkModel)
+
+        save_object("final_model/model.pkl",best_model)
        
         
 
